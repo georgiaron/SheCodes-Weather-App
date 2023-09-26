7b47e28045dfcc14cf34c0774c6488212fbed971
georgiaron <69826104+georgiaron@users.noreply.github.com>
2023-09-26 17:18:34 +1300

    Update JS - API working
    Store response from the api in a variable called Forecast. It looks through this variable and it shows the next upcoming days including the current day.
    
    Using the object and index for each function to format the day of the time stamp using a function I created. Includes the icon name and rounding the temperature for max and min temperatures.
    
    Set up the index so that it only shows 6 days.
    
    Week 8 Lesson 6.

--- src/index.js
+++ src/index.js
@@ -43,24 +43,41 @@ function formatDate(timestamp) {
   return formattedDate;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+  console.log(forecast);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+    <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+
+    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+      forecastDay.condition.icon
+    }.png" alt="" width="42" />
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max">18 &deg;</span>
-      <span class="weather-forecast-temperature-min">12 &deg;</span>
+      <span class="weather-forecast-temperature-max">${Math.round(
+        forecastDay.temperature.maximum
+      )} &deg;</span>
+      <span class="weather-forecast-temperature-min">${Math.round(
+        forecastDay.temperature.minimum
+      )} &deg;</span>
     </div>
   </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `<div>`;
